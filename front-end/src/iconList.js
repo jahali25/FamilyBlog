5976103f68330fd15543ce0d3b33681ba9f4a71f
Kinamo Williams-Laptop <wkinamo@gmail.com>
2021-04-15 21:44:28 -0600

    Added the icons for the users to choose

---
+++ front-end/src/iconList.js
@@ -0,0 +1,45 @@
+const photoPath = "/icons/";
+
+icons = [{
+    id: 1,
+    name: "basketball",
+    path: photoPath + "basketball.svg"
+},
+{
+    id: 2,
+    name: "baseball",
+    path: photoPath + "baseball.svg"
+},
+{
+    id: 3,
+    name: "football",
+    path: photoPath + "footballball.svg"
+},
+{
+    id: 4,
+    name: "smileyFace",
+    path: photoPath + "smileyFace.svg"
+},
+{
+    id: 5,
+    name: "heart",
+    path: photoPath + "heart.svg"
+},
+{
+    id: 6,
+    name: "fullSend",
+    path: photoPath + "fullSend.svg"
+},
+{
+    id: 7,
+    name: "spider",
+    path: photoPath + "spider.svg"
+},
+{
+    id: 8,
+    name: "lightning",
+    path: photoPath + "baseball.svg"
+}
+]
+
+export default icons;
--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import icons from './iconList.js'
 
 Vue.config.productionTip = false
 
+let data = {
+  user: null,
+  isAdmin: false,
+  icons: icons
+}
+
 new Vue({
+  data,
   router,
   render: h => h(App)
 }).$mount('#app')
